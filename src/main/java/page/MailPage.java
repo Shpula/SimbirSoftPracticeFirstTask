b965815b62dd882740c55f19b27af7914c924dd9
butin <butin@DESKTOP-63BCB89>
2022-02-28 19:06:58 +0400

    done

--- src/main/java/page/MailPage.java
+++ src/main/java/page/MailPage.java
@@ -16,7 +16,7 @@ public class MailPage extends BasePage{
         super(driver);
     }
 
-    ArrayList<Integer> count = new ArrayList<>();
+    int newCountMails = 0;
 
     @FindBy(xpath = "//span[text() = 'Написать']")
     WebElement createMailButton;
@@ -33,26 +33,24 @@ public class MailPage extends BasePage{
     @FindBy(xpath = "//button[@class='Button2 Button2_pin_circle-circle Button2_view_default Button2_size_l']")
     WebElement sendButton;
 
-    public List<Integer> findsСurrentMailsCase() {
+    public Integer findsСurrentMailsCase() {
         int mails = driver.findElements(By.xpath("//span[@title='Simbirsoft theme']")).size();
-        count.add(mails);
-        System.out.println(count);
-        return count;
+        return mails;
     }
 
     public MailPage sendMailCase() {
         createMailButton.click();
         mail.sendKeys("butin.nik.test@yandex.ru", Keys.ENTER);
         theme.sendKeys("Simbirsoft theme");
-        mainMail.sendKeys("Найдено писем: " + findsСurrentMailsCase().get(0));
+        mainMail.sendKeys("Найдено писем: " + findsСurrentMailsCase());
         sendButton.click();
-        findsСurrentMailsCase().add(1, findsСurrentMailsCase().get(0) + 1);
+        newCountMails = findsСurrentMailsCase() + 1;
 
         return this;
     }
 
     public MailPage checkCountMails() {
-        Assertions.assertTrue(findsСurrentMailsCase().get(0) < findsСurrentMailsCase().get(1));
+        Assertions.assertTrue(findsСurrentMailsCase() < newCountMails);
         return this;
     }
 }
